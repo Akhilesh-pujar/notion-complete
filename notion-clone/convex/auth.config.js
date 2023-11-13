49e01df7ab306590d3377920927892fa5c78bde2
Akhilesh <akhileshpujar796@gmail.com>
2023-11-13 16:07:13 +0530

    authbugfixed

--- notion-clone/convex/auth.config.js
+++ notion-clone/convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://maximum-stinkbug-27.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
