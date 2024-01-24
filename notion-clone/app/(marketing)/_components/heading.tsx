16c6e432b46568c295601e31c1bfa91ddb384184
Akhilesh <akhileshpujar796@gmail.com>
2024-01-24 19:30:11 +0530

    few-new-changes

--- notion-clone/app/(marketing)/_components/heading.tsx
+++ notion-clone/app/(marketing)/_components/heading.tsx
@@ -17,7 +17,7 @@ export const Heading = () => {
         Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br />
+       NextGen Notes is the connected workspace where <br />
         better, faster work happens.
       </h3>
       {isLoading && (
@@ -28,7 +28,7 @@ export const Heading = () => {
       {isAuthenticated && !isLoading && (
         <Button asChild>
           <Link href="/documents">
-            Enter Jotion
+            Enter IdeaForge: NextGen Notes
             <ArrowRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
@@ -36,7 +36,7 @@ export const Heading = () => {
       {!isAuthenticated && !isLoading && (
         <SignInButton mode="modal">
           <Button>
-            Get Jotion free
+            Get NextGen Notes taking app for free
             <ArrowRight className="h-4 w-4 ml-2" />
           </Button>
         </SignInButton>
